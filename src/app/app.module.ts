3987cfd6a47f9c3bf18cf23bd54e115a121639f9
-KaiK- <enriqueoriol@gmail.com>
2017-12-16 16:43:58 +0100

    added Angular SSR module and express server file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +12,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId:'MySampleProject'})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string
+  ){
+    const platform = isPlatformBrowser(this.platformId) ? 'browser' : 'server';
+    console.log("I'm on the ", platform);
+  }
+ }
