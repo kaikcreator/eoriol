0598adbed09035e78b1b1eb2820e38990c4c7e8a
-KaiK- <enriqueoriol@gmail.com>
2017-12-20 17:24:01 +0100

    added basic navigation and layout to the main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId:'MySampleProject'})
+    BrowserModule.withServerTransition({appId:'MySampleProject'}),
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+
+export const appRoutes: Routes = [
+    // { path: 'post/:id',      component: PostComponent },
+    {
+      path: 'home',
+      component: HomeComponent,
+      data: { title: 'Enrique Oriol' }
+    },
+    { path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    },
+    { path: '**', component: PageNotFoundComponent }
+  ];
