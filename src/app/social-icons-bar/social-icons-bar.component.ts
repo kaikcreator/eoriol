c9094efeb619036f18853d065ec018cea71c3a76
-KaiK- <enriqueoriol@gmail.com>
2018-02-07 16:46:51 +0100

    Moved social icons to its own component, to reuse them between mobile-menu and home contact section Solved issue with SVG old xml syntax breaking stuff with Angular Universal

---
+++ src/app/social-icons-bar/social-icons-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-icons-bar',
+  templateUrl: './social-icons-bar.component.html',
+  styleUrls: ['./social-icons-bar.component.scss']
+})
+export class SocialIconsBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
