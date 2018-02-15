5e655cf0d92460f0d20caee5ad8551311f1d6131
-KaiK- <enriqueoriol@gmail.com>
2018-02-15 15:48:49 +0100

    minor changes adding some more classes to handle wordpress items

--- src/app/services/wordpress.service.ts
+++ src/app/services/wordpress.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { map, flatMap } from 'rxjs/operators';
 import { PostModel } from '../models/post.model';
 import { HttpClient } from '@angular/common/http';
+import { WpPost } from '../models/wp/wp-post.model';
 
 
 @Injectable()
@@ -13,14 +14,15 @@ export class WordpressService {
     return this.http.get<any[]>("http://blog.enriqueoriol.com/wp-json/wp/v2/posts?_embed&per_page="+n)
       .pipe(
         map(data => {
-          console.log(data);
+          /* this must be an array of posts*/
           return data.map(item => {
-            console.log(item);
+            let wpItem = new WpPost(item);
+
             return <PostModel>{
-              date: item.date,
-              title: item.title.rendered,
-              link: item.link,
-              image: item._embedded['wp:featuredmedia'][0].source_url
+              date: wpItem.date(),
+              title: wpItem.title(),
+              link: wpItem.link(),
+              image: wpItem.featuredImage()
             }})
         })
       );
