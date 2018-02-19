861bc487504935891862fa2517bc34c1f9f480ac
-KaiK- <enriqueoriol@gmail.com>
2018-02-20 00:07:08 +0100

    Solved issue with unsuported mix-blend-mode Added animation on subscribe-cta button clicked Hidden subscribe-cta on small and medium screens

--- src/app/subscribe-cta/subscribe-cta.component.ts
+++ src/app/subscribe-cta/subscribe-cta.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-subscribe-cta',
   templateUrl: './subscribe-cta.component.html',
-  styleUrls: ['./subscribe-cta.component.scss']
+  styleUrls: ['./subscribe-cta.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        'opacity': '0',
+        'transform': 'translateX(10%)'
+      })),
+      transition('* => *', [
+        animate('200ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class SubscribeCtaComponent implements OnInit {
 
