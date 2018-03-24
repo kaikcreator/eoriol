991149f4f282d3e19dd492f80254378c19073eed
-KaiK- <enriqueoriol@gmail.com>
2018-03-24 23:36:07 +0100

    changed https for http wp api call to detect if SSR issue is caused because of that

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://enriqueoriol.com/assets/fixtures',
-  wordpressUrl: 'https://blog.enriqueoriol.com/wp-json/wp/v2',
+  wordpressUrl: 'http://blog.enriqueoriol.com/wp-json/wp/v2',
   mailchimpUrl: 'https://enriqueoriol.us12.list-manage.com',
   contactUrl: 'https://blog.enriqueoriol.com/wp-json/contact-form-7/v1/contact-forms/1474/feedback'
 };
