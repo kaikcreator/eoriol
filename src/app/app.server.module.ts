6a127ee1ba31a6682ce7ae40a87d175871f48707
-KaiK- <enriqueoriol@gmail.com>
2018-04-13 19:05:27 +0200

    created app shell. Universal renders now index-1.html with app shell, but unexpectedly the service worker stoped caching files

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,17 +4,28 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
  
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+const routes: Routes = [ { path: 'shell', component: AppShellComponent }, ...appRoutes()];
  
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
-    ServerTransferStateModule
+    ServerTransferStateModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     // Add universal-only providers here
   ],
   bootstrap: [ AppComponent ],
+  declarations: [AppShellComponent],
 })
-export class AppServerModule {}
+export class AppServerModule {
+  constructor(private router:Router){
+    this.router.resetConfig(routes);
+  }
+}
