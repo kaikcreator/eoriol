7b8e101aba3b88f10a99d605beccab72f428f28a
-KaiK- <enriqueoriol@gmail.com>
2018-05-16 16:45:26 +0200

    added spinner while post list is loading

--- src/app/ui-common/ui-common.module.ts
+++ src/app/ui-common/ui-common.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogCardComponent } from './blog-card/blog-card.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BlogCardComponent],
-  exports: [BlogCardComponent]
+  declarations: [BlogCardComponent, SpinnerComponent],
+  exports: [BlogCardComponent, SpinnerComponent]
 })
 export class UiCommonModule { }
