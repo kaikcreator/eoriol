c25b382122111ef9c59d4718137609306986b969
-KaiK- <enriqueoriol@gmail.com>
2018-06-01 00:29:00 +0200

    fixed PostModel to include new fields, updated blog-card component in order to use the right type for item (PostModel), and updated prettify to prevent crashing when loading code in server environment

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,9 +1,11 @@
 
 
 export class PostModel {
-    public title:string;
     public date:string;
+    public title:string;
+    public slug:string;//unique name as identifier of the article
+    public path:string;//local path to the post
+    public link:string;//absolute path to the post in wordpress site
     public image:string;
-    public link:string;
     public content?:string;
 }
