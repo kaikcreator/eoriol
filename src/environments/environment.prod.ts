507410089650985c1c8eec2f181ecfe38ac2a49c
-KaiK- <kaikcreator@users.noreply.github.com>
2019-08-23 00:41:57 +0200

    Updated book courses json to v1.3 and updated bio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   googleAnalyticsId: 'UA-2769438-8',
+  coursesJsonVersion: 'v1.3',
   apiUrl: 'https://enriqueoriol.com/assets/fixtures',
   wordpressUrl: 'https://blog.enriqueoriol.com/wp-json/wp/v2',
   wordpressCustomUrl: 'https://blog.enriqueoriol.com/wp-json/custom/v1',
